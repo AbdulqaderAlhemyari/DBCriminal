cba0f757e952dc4e847347fcc942056e7baa2280
Abdulqader <alhemyariabdulqader@gmail.com>
2020-10-20 10:21:56 +0300

    enhancing performane of recyclerview

---
+++ app/src/main/java/com/example/dbcriminal/Crime.kt
@@ -0,0 +1,40 @@
+package com.example.dbcriminal
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false )
+//changes due to doing challenge no 11
+{
+    override fun equals(other : Any?): Boolean
+    {
+        if(javaClass != other?.javaClass)
+        {
+            return false
+        }
+
+        other as Crime
+        if(id == other.id)
+        {
+            return false
+        }
+        if(title == other.title)
+        {
+            return false
+        }
+        if(date == other.date)
+        {
+            return false
+        }
+        if(isSolved == other.isSolved) {
+            return false
+        }
+        return true
+    }
+
+}
+
---
+++ app/src/main/java/com/example/dbcriminal/CrimeListFragment.kt
@@ -0,0 +1,174 @@
+package com.example.dbcriminal
+
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.text.DateFormat
+import java.util.*
+
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment : Fragment() {
+
+    /**
+     * Required interface for hosting activities
+     */
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+    private var callbacks: Callbacks? = null
+
+    private lateinit var crimeRecyclerView: RecyclerView
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }*/
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        crimeRecyclerView =
+            view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        crimeRecyclerView.adapter = adapter
+        //updateUI()
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+            })
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+
+    //private fun updateUI() {
+    private fun updateUI(crimes: List<Crime>) {
+        /*val crimes = crimeListViewModel.crimes*/
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+        //changes due to doing challenge no 11
+        val adapterV = crimeRecyclerView.adapter as CrimeAdapter
+
+        adapterV.submitList(crimes)
+    }
+
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),View.OnClickListener {
+        private lateinit var crime: Crime
+
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = DateFormat.getDateInstance(DateFormat.FULL).format(this.crime.date).toString()
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
+
+        override fun onClick(v: View) {
+
+            callbacks?.onCrimeSelected(crime.id)
+        }
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+                : CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(view)
+        }
+
+        override fun getItemCount() = crimes.size
+       //changes due to doing challenge no 11
+        fun submitList(vCrime:List<Crime>)
+        {
+            val oldList=crimes
+            val diffResult : DiffUtil.DiffResult = DiffUtil.calculateDiff(
+                CrimeDiffUtilCallBack(oldList,vCrime))
+            crimes=vCrime
+            diffResult.dispatchUpdatesTo(this)
+        }
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.bind(crime)
+        }
+
+        //changes due to doing challenge no 11
+        inner class CrimeDiffUtilCallBack(var oldCrime: List<Crime>, var newCrime: List<Crime>):
+                DiffUtil.Callback()
+        {
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                        return (oldCrime.get(oldItemPosition).id == newCrime.get(newItemPosition).id)
+            }
+
+            override fun getOldListSize(): Int {
+                    return oldCrime.size
+            }
+
+            override fun getNewListSize(): Int {
+                    return newCrime.size
+            }
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean
+            {
+                return oldCrime.get(oldItemPosition).equals(newCrime.get(newItemPosition))
+            }
+
+        }
+    }
+}
